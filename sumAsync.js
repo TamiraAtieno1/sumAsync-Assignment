2f881af6cac4ded99619f7008b6bcb2070622509
TamiraAtieno1 <tamiraatieno20@gmail.com>
2023-06-22 16:25:47 +0300

    created sumAsync

--- sumAsync.js
+++ sumAsync.js
@@ -1,12 +1,26 @@
-function sumAsync () {
-    num1 = 10
-    num2 = 20
-    let result = num1 + num2
 
-    setTimeout(() => {
-        console.log(result)
-    },5000)
+// function sumAsync () {
+//     num1 = 10
+//     num2 = 20
+//     let result = num1 + num2
 
-}
+//     setTimeout(() => {
+//         console.log(result)
+//     },5000)
+// 
+// }
 
 sumAsync()
+
+// real solution
+function sumAsync(num1, num2, callback) {
+    setTimeout(() => {
+        return callback(num1 + num2)
+    }, 5000)
+}
+
+sumAsync(60, 50, (result) => {
+    console.log(result)
+})
+
+
